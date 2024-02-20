f26c4079c9c119196b4ec454387ff2da7b2a2d4b
krish <guptashanu341@gmail.com>
2024-02-20 20:00:37 +0530

    twentyone commit

---
+++ 500 rating to 1000 rating question/Good Program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int t,n;
+scanf("%d",&t);
+for(int i=0;i<t;i++){
+    scanf("%d",&n);
+    if(n%4==0){
+        printf("\nGood");
+    }
+    else{
+        printf("\nNot Good");
+    }
+    
+    
+}
+    
+}
+
