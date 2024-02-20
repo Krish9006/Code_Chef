97c6ce1e0d554405a270119024f85db498f3eb76
krish <guptashanu341@gmail.com>
2024-02-20 19:50:44 +0530

    fourtheen commit

---
+++ 500 rating to 1000 rating question/The Three Topics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+ int a,b,c,x;
+        scanf("%d %d %d %d",&a,&b,&c,&x);
+        if(x==a || x==b || x==c)
+        printf("YES \n");
+        else
+        printf("NO \n");
+       
+        
+	// your code goes here
+	return 0;
+}
+
