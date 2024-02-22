d53508d82f220efbc7f0ac833eda4a6b80127081
krish <guptashanu341@gmail.com>
2024-02-22 22:17:19 +0530

    thirtythree commit

---
+++ 500 rating to 1000 rating question/Small factorials.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,r;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+	        scanf("%d%d",&n,&r);
+	        printf("%d\n",n!/n!(n-r));
+        
+}	
+
+}
+
