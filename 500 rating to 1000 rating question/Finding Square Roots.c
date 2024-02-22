de9010106a933f5654a5c03e460eb4762411a92a
krish <guptashanu341@gmail.com>
2024-02-22 22:21:08 +0530

    thirtyeight commit

---
+++ 500 rating to 1000 rating question/Finding Square Roots.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++)
+{
+       int n,r;
+	    scanf("%d",&n);  
+	    r=sqrt(n);
+	    printf("\n%d",r);
+	}
+	return 0;
+        
+}
+
+
