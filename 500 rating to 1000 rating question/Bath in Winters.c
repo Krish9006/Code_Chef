6230f3168c30df9a2fff63ade1108f416d427511
krish <guptashanu341@gmail.com>
2024-02-22 22:15:32 +0530

    thirtyone commit

---
+++ 500 rating to 1000 rating question/Bath in Winters.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int t,x,y,z;
+	
+	scanf("%d",&t);
+	
+	while(t--){
+	    scanf("%d%d",&x,&y);
+	    z = y * 2;
+	    if(x < z){
+	        printf("%d\n",0);
+	    }
+	    else{
+	        printf("%d\n",x / z);
+	    }
+	    
+	}
+	return 0;
+
+}
+
