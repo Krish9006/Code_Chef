f05abd3d0ea69fbd331a42446b1b437e5bedcc62
krish <guptashanu341@gmail.com>
2024-02-25 20:19:32 +0530

    fourtyone commit

---
+++ 500 rating to 1000 rating question/Fill Candies.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n,k,m;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+	        scanf("%d%d%d",&n,&k,&m);
+	if(n%(k*m)==0){
+	        printf("%d\n",n/(k*m));
+	    }
+	    else{
+	        printf("%d\n",(n/(k*m))+1);
+	    }
+	}
+	return 0;
+
+}
+
