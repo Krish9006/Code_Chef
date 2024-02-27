044381b7189a12f3c71f76f91e4af0cd594ed946
krish <guptashanu341@gmail.com>
2024-02-27 12:02:29 +0530

    fiftyfive commit

---
+++ 500 rating to 1000 rating question/A or B.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y,c,d;
+	    scanf("%d%d",&x,&y); 
+	    c=(500-(2*x)+1000-(4*(y+x)));
+	    d=(1000-(4*y)+500-(2*(y+x)));
+	        if(c>d){
+	    printf("%d\n",c);}
+	
+	else { printf("%d\n",d);}}
+	return 0;
+}
+
