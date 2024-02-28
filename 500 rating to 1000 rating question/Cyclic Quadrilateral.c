bb9d4cc2b46aa9be93fdce6303e6fb06dd4f9357
krish <guptashanu341@gmail.com>
2024-02-28 23:34:38 +0530

    fiftyeight commit

---
+++ 500 rating to 1000 rating question/Cyclic Quadrilateral.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int t;
+	scanf("%d",&t);
+	while(t--){
+	    int a,b,c,d;
+	    scanf("%d %d %d %d",&a,&b,&c,&d);
+	    if ( a+c==180 && b+d ==180)
+	    printf("yes\n");
+	    else printf("no\n");
+	    
+	}
+	return 0;
+}
+
