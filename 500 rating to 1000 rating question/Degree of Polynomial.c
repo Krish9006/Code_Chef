0a9e172f989e4cf0f57bf1f0cad4175a3daf3b65
krish <guptashanu341@gmail.com>
2024-03-03 12:07:03 +0530

    seventyfour commit

---
+++ 500 rating to 1000 rating question/Degree of Polynomial.c
@@ -0,0 +1,10 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    c=list(map(int,input().split()))
+    degree=0
+    for i in range(len(c)):
+        if c[i] != 0 and i>degree:
+            degree=i 
+    print(degree)
