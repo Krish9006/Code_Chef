7572cc47b7315505860d6c9147edb58f159bbcd1
krish <guptashanu341@gmail.com>
2024-03-12 21:52:53 +0530

    four commit

---
+++ cup finals.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,n,i,A[100];
+	
+	std::cin >> t;
+	
+	while(t--)
+	{    
+	    int x=0;
+	    cin>>n;
+	       for(i=0; i<n; i++)
+	       {
+	           cin>>A[i];
+	       }
+	       for(i=0; i<n; i++){
+	          
+              
+	       }
+	      cout<<x<<endl;
+          
+		}
+	
+	return 0;
+}
