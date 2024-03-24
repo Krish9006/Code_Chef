1bd9b975e8f4ba674c9c5d2820b4cd17e04b5ea0
krish <guptashanu341@gmail.com>
2024-03-24 19:58:39 +0530

    eight commit

---
+++ 500 rating to 1000 rating question/bear_and_big_brother.cpp
@@ -0,0 +1,18 @@
+
+# include <bits/stdc++.h>
+using namespace std;
+// Solved
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int count=0;
+    while(a<=b){
+        a*=3;
+        b*=2;
+        count+=1;
+    }
+    cout<<count;
+    
+
+}
