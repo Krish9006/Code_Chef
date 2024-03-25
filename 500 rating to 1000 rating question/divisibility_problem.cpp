bd86de474227b0d67872471884c6c78068af8e8c
krish <guptashanu341@gmail.com>
2024-03-25 21:57:02 +0530

    twele commit

---
+++ 500 rating to 1000 rating question/divisibility_problem.cpp
@@ -0,0 +1,22 @@
+# include <bits/stdc++.h>
+using namespace std;
+// Solved
+int main()
+{
+    int t;
+    cin>>t;
+    while(t!=0)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a%b==0)
+        {
+            cout<<0<<'\n';
+        }
+        else
+        {
+            cout<<b-(a%b)<<'\n';
+        }
+        t--;
+    }
+}
