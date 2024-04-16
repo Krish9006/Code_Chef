80023d836d7d6b0e9d26c548446ea27e0524c5f6
krish <guptashanu341@gmail.com>
2024-04-16 19:30:41 +0530

    third commit

---
+++ python code bootcamp.py/Calculate abd diff of two number.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_of_numbers = num1 + num2
+difference_of_numbers = num1 - num2
+
+print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
+print(f"The difference between {num1} and {num2} is: {difference_of_numbers}")
